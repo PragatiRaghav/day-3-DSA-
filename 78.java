2a4ac2f122fde7b26cc7feccc4987f34c694f616
PragatiRaghav <pragatiraghav2006@gmail.com>
2026-09-02 17:49:48 +0530

    Create 78.java
    78. Subsets
    Solved
    Medium
    
    Topics
    premium lock icon
    Companies
    Given an integer array nums of unique elements, return all possible subsets (the power set).
    
    The solution set must not contain duplicate subsets. Return the solution in any order.

---
+++ 78.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        backtrack(nums, 0, new ArrayList<>());
+        return ans;
+    }
+
+    private void backtrack(int[] nums, int index, List<Integer> curr) {
+        
+        ans.add(new ArrayList<>(curr));
+
+        for (int i = index; i < nums.length; i++) {
+            curr.add(nums[i]);             
+            backtrack(nums, i + 1, curr); 
+            curr.remove(curr.size() - 1); 
+        }
+    }
+}
